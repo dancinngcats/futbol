8e5e8db824c7432bd80520c09be26a1103fc2a27
r-stemmle <ryan.stemmle@gmail.com>
2021-03-03 11:44:30 -0500

    Refactor to manager classes

--- test/teams_manager_test.rb
+++ test/teams_manager_test.rb
@@ -15,4 +15,15 @@ class TeamsManagerTest < Minitest::Test
     teams_manager = TeamsManager.new(setup)
     assert_equal 32, teams_manager.count_of_teams
   end
+
+  def test_it_can_get_team_info
+    teams_manager = TeamsManager.new(setup)
+    expected = {"team_id"=>"3",
+                "franchise_id"=>"10",
+                "team_name"=>"Houston Dynamo",
+                "abbreviation"=>"HOU",
+                "link"=>"/api/v1/teams/3"
+              }
+    assert_equal expected, teams_manager.team_info("3")
+  end
 end
